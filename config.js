7e15170363d0da04854211f0d9172bc72af2d67a
acudriz <acudriz@tie-in.co>
2017-06-01 11:22:48 -0500

    mlab

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Fichero de configuración de la bd y puertos
 module.exports ={
 	port: process.env.PORT || 3001,
-	db: process.env.MONGODB || 'mongodb://usuarioevo:123@localhost/evohr',
+	db: process.env.MONGODB || 'mongodb://admin:admin@ds157641.mlab.com:57641/heroku_xtldxzhc',
 	SECRET_TOKEN: 'miclavedetokens'
 }
  //nube - mongodb://admin:admin@ds157641.mlab.com:57641/heroku_xtldxzhc
