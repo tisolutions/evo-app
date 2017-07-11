dfd9b3f6349ecf9c6484859706669909ce42826c
Jaime Morales <jmorales@tie-in.co>
2017-07-11 09:30:29 -0500

    Adicion del FTP para el controlador empresa, Back-end

--- Controlador/cBackend/cb.empresa.js
+++ Controlador/cBackend/cb.empresa.js
@@ -10,89 +10,108 @@ var routerEmpresa = express.Router()
 const JSFtp = require("jsftp")
 const Ftp = new JSFtp({
   host: "107.170.78.97",
-  port: 21, // defaults to 21 
-  user: "ftpuser", // defaults to "anonymous" 
-  pass: "tiein2017" // defaults to "@anonymous" 
+  port: 21, // defaults to 21
+  user: "ftpuser", // defaults to "anonymous"
+  pass: "tiein2017" // defaults to "@anonymous"
 })
 
 
 routerEmpresa.route("/empresas")
-.get(function(req, res) {
-  modeloEmpresa.find().limit(1)
-   .then((empresa) =>{
-       res.send(empresa)
-   })
-   .catch((err) =>{
-     message: "Error al tratar de traer los registros"
-   });
-})
+  .get(function(req, res) {
+    modeloEmpresa.find().limit(1)
+     .then((empresa) =>{
+         res.send(empresa)
+     })
+     .catch((err) =>{
+       message: "Error al tratar de traer los registros"
+     });
+  })
+  .post(upload.any(),function(req,res){
+    var data = new modeloEmpresa({
+        nit : req.body.nit,
+        direccion : req.body.direccion,
+        telefono : req.body.telefono,
+        razonSocial : req.body.razonSocial,
+        correoElectronico : req.body.correoElectronico,
+        paginaweb : req.body.paginaweb
+    });
 
-.post(upload.any(),function(req,res){
+    data.save()
+    .then((empresa) =>{
+        if (req.files) {
+            req.files.forEach(function(file){
+            var ext = file.originalname;
+            ext = ext.substring(ext.lastIndexOf('.'));
+            var filename = empresa._id+ext;
+            var remotePath = 'evoHR/empresa/'+filename;
 
+            Ftp.put(file.path, remotePath, function(hadError) {
+                if (!hadError)
+                  console.log("File transferred successfully!");
+                  res.status(200).send(empresa);
+            });
+          })
+        }
+    })
 
-  var data = new modeloEmpresa({
-      nit : req.body.nit,
-      direccion : req.body.direccion,
-      telefono : req.body.telefono,
-      razonSocial : req.body.razonSocial,
-      correoElectronico : req.body.correoElectronico,
-      paginaweb : req.body.paginaweb
+    .catch((error)=>{
+      res.status(500).send(error);
+    })
+  })
+  .put(upload.any(),function(req,res){
+    modeloEmpresa.findByIdAndUpdate(req.query.id, req.body)
+    .then((empresa) =>{
+      if (req.files.length > 0) {
+            req.files.forEach(function(file){
+              var ext = file.originalname;
+              ext = ext.substring(ext.lastIndexOf('.'));
+              var filename = EmpresaId+ext;
+              var remotePath = 'evoHR/empresa/'+filename;
+
+              Ftp.put(file.path, remotePath, function(hadError) {
+                  if (!hadError)
+                    console.log("File transferred successfully!");
+                  // Ftp.chmod(remotePath, 511);
+                  res.status(200).send(empresa);
+              });
+            })
+      }else{
+          res.status(200).send(empresa);
+      }
+    })
+
+    .catch((err)=>{
+      res.status(500).send({
+        error: err
+      });
+    })
   });
 
-  data.save()
-  .then((empresa) =>{
-      if (req.files) {
-          req.files.forEach(function(file){
-          var ext = file.originalname;
-          ext = ext.substring(ext.lastIndexOf('.'));
-          var filename = empresa._id+ext;
-          var remotePath = 'evoHR/empresa/'+filename;
+routerEmpresa.route("/empresas-adicion-config")
+  .put(upload.any(),function(req,res){
+    modeloEmpresa.findByIdAndUpdate(req.query.id, req.body)
+    .then((empresa) =>{
+      res.status(200).send(empresa);
+    })
 
-          Ftp.put(file.path, remotePath, function(hadError) {
-              if (!hadError)
-                console.log("File transferred successfully!");
-                res.status(200).send(empresa);
-          });
-        })
-      }
-  })
+    .catch((err)=>{
+      res.status(500).send({
+        error: err
+      });
+    })
+  });
 
-  .catch((error)=>{
-    res.status(500).send(error);
-  })
-})
-
-.put(upload.any(),function(req,res){
-  let EmpresaId = req.body.id
-  let body = req.body
-
-  modeloEmpresa.findByIdAndUpdate(EmpresaId, body)
-  .then((empresa) =>{
-    if (req.files.length > 0) {
-          req.files.forEach(function(file){
-          var ext = file.originalname;
-          ext = ext.substring(ext.lastIndexOf('.'));
-          var filename = EmpresaId+ext;
-          var remotePath = 'evoHR/empresa/'+filename;
-
-          Ftp.put(file.path, remotePath, function(hadError) {
-              if (!hadError)
-                console.log("File transferred successfully!");
-              // Ftp.chmod(remotePath, 511);
-              res.status(200).send(empresa);
-          });
-        })
-      }else{
+  routerEmpresa.route("/empresas-contrato-usuario")
+    .get(upload.any(),function(req,res){
+      modeloEmpresa.findByIdAndUpdate(req.query.id, req.body)
+      .then((empresa) =>{
         res.status(200).send(empresa);
-      }
-  })
+      })
 
-  .catch((err)=>{
-    res.status(500).send({
-      error: err
+      .catch((err)=>{
+        res.status(500).send({
+          error: err
+        });
+      })
     });
-  })
-})
-
-
-module.exports = routerEmpresa;
+  module.exports = routerEmpresa;
