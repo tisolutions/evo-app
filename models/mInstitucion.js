255e22ec71a16032895acee609800e19ad516571
Jaime Morales <jmorales@tie-in.co>
2017-07-11 09:43:32 -0500

    Adicion del modelo Institucion, Mongo

---
+++ models/mInstitucion.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
+
+const institucionSchema = new Schema({
+    codigo: {
+    	type: Number
+    },
+    nombre: {
+    	type: String,
+    	require: "Escriba el nombre de la Institucion"
+    }
+});
+
+module.exports = mongoose.model('Institucion', institucionSchema)
